ac1b4164bbd035a197f47043d35cfa7e220ed49a
Pedro Almeida <86891854+PedroJCAlmeida@users.noreply.github.com>
2025-05-26 08:42:43 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 //export default { root: './' }
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
   server: {
     historyApiFallback: true
