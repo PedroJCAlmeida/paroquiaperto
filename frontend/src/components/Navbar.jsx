657daddcefcdbf8ef55369243417d5744d425c97
Pedro Almeida <pedrojoaocarvalhoalmeida@gmail.com>
2025-09-14 12:25:03 +0100

    Refactor Navbar component: clean up code structure and ensure isLoggedIn state is defined correctly

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import logo from '../assets/logo.png';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
+  const isLoggedIn = Boolean(localStorage.getItem('token'));
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -17,9 +18,6 @@ const Navbar = () => {
         <Link to="/" className="navbar-logo" onClick={() => setIsOpen(false)}>
           <img src={logo} alt="Paróquia Perto" />
         </Link>
-
-         
-        
         <button
           className={isOpen ? 'active navbar-toggle' : 'navbar-toggle'}
           onClick={toggleMenu}
@@ -29,18 +27,21 @@ const Navbar = () => {
           <span className="hamburger"></span>
           <span className="hamburger"></span>
         </button>
-
         <nav className={`navbar-nav ${isOpen ? 'active' : ''}`}>
           <Link to="/" className="navbar-link" onClick={() => setIsOpen(false)}>Início</Link>
           <Link to="/paroquias" className="navbar-link" onClick={() => setIsOpen(false)}>Paróquias</Link>
           <Link to="/buscar" className="navbar-link" onClick={() => setIsOpen(false)}>Buscar</Link>
           <Link to="/contato" className="navbar-link" onClick={() => setIsOpen(false)}>Contato</Link>
-       <Link to="/login" className="navbar-login-icon" title="Login" onClick={() => setIsOpen(false)}>
-          <FaUserCircle size={28} />
-         </Link>
+          <Link to="/login" className="navbar-login-icon" title="Login" onClick={() => setIsOpen(false)}>
+            <FaUserCircle size={28} />
+          </Link>
         </nav>
-
-        
+        {/* Renderiza o menu do backoffice se logado */}
+        {isLoggedIn && (
+          <div style={{ position: 'absolute', top: 0, right: 0, zIndex: 100 }}>
+            {require('./BackofficeMenu.jsx').default()}
+          </div>
+        )}
       </div>
     </header>
   );
