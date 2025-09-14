51923eceda78a98f63baad735233d36be043b8bd
Pedro Almeida <pedrojoaocarvalhoalmeida@gmail.com>
2025-09-14 13:10:41 +0100

    Refactor BackofficeLayout to import BackofficeNavbar directly instead of using require

--- frontend/src/components/BackofficeLayout.jsx
+++ frontend/src/components/BackofficeLayout.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import BackofficeMenu from './BackofficeMenu';
+import BackofficeNavbar from './BackofficeNavbar.jsx';
 import '../styles/BackofficeLayout.css';
 
 export default function BackofficeLayout() {
-  const BackofficeNavbar = require('./BackofficeNavbar.jsx').default;
   return (
     <div className="backoffice-container">
       <BackofficeNavbar />
